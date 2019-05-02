2fb160d109f0017c28192e2e9974f105ae1fc0fc
Johnnylv89 <44009127+Johnnylv89@users.noreply.github.com>
2019-05-02 23:13:01 +0300

    Add files via upload

---
+++ giraff dance.py
@@ -0,0 +1,18 @@
+class Girafes:
+    def left_foot_forward(self):
+        print "Left foot forward"
+        pass
+    def left_foot_back(self):
+        print "Left foot back"
+        pass
+    def right_foot_forward(self):
+        print "Right foot forward"
+        pass
+    def right_foot_back(self):
+        print "Right foot back"
+        pass
+    def dance(self):
+        self.left_foot_forward()
+        self.left_foot_back()
+        self.right_foot_back()
+        self.right_foot_forward()
